c7404018589322eca4c1506d2bf42908143c50a3
SenoOh <pwmj9579@s.okayama-u.ac.jp>
2024-08-20 14:08:55 +0900

    Fix model test about action_item

--- app/models/action_item.rb
+++ app/models/action_item.rb
@@ -1,5 +1,9 @@
 class ActionItem < ApplicationRecord
-  after_create :set_uid
+  before_create :check_varid_id
+  before_create :set_uid
+  validates :task_url, format: { with: /\A[a-zA-Z0-9\-._~:\/?#\[\]@!$&'()*+,;%=]+\z/}, allow_nil: true
+  validates :uid, uniqueness: true
+  before_update :check_uid_readonly
 
   def uid
     "%04d" % self.id
@@ -7,7 +11,19 @@ class ActionItem < ApplicationRecord
 
   private
 
+  def check_varid_id
+    if self.id > 9999 || self.id < 1 
+      throw(:abort)
+    end
+  end
+
   def set_uid
-    self.update(uid: uid)
+    self.uid = uid
+  end
+
+  def check_uid_readonly
+    if uid_changed?
+      throw(:abort)
+    end
   end
 end
