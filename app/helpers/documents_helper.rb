6c4b67fa6c1fd9c3e692ae83bb0d7e198d467f42
fujiwara-e <pjxk6clf@s.okayama-u.ac.jp>
2026-03-24 14:09:17 +0900

    Add support for responsive design to tasks and documents

--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -12,4 +12,27 @@ module DocumentsHelper
     end
     forms.join.html_safe
   end
+
+  def sort_options
+    [
+      { value: 'content_asc', label: 'タイトル順（昇順）' },
+      { value: 'content_desc', label: 'タイトル順（降順）' },
+      { value: 'updated_at_asc', label: '更新順（古い順）' },
+      { value: 'updated_at_desc', label: '更新順（新しい順）' },
+      { value: 'created_at_asc', label: '作成順（古い順）' },
+      { value: 'created_at_desc', label: '作成順（新しい順）' },
+      { value: 'start_at_asc', label: '開始時刻順（昇順）' },
+      { value: 'start_at_desc', label: '開始時刻順（降順）' }
+    ]
+  end
+
+  def current_sort_value(q)
+    current_sort = q.sorts.first&.name
+    if current_sort.present?
+      direction = q.sorts.first&.dir
+      "#{current_sort}_#{direction}"
+    else
+      'content_asc'
+    end
+  end
 end
